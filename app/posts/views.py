ca3e67d239a76aaaa36d726bf2cdd4432f4dd850
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-26 22:23:30 +0900

    posts/views.py post_list view setting;

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'post':posts,
+    }
+
+    return render(request, 'posts/post_list.html', context)
+
+    pass
+
+def post_create(request):
+    pass
+
+def post_edit(request):
+    pass
+
+def recent_post(request):
+    pass
+
