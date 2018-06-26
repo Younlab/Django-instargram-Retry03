6f90e731faffcf95e16b78d3855c3254e4a46cb7
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-26 22:22:55 +0900

    posts/models.py Post models setting

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='post_images', blank=True)
+    content = models.TextField(blank=True)
+    created_at = models.TimeField(auto_now_add=True)
+
