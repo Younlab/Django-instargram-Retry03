78cb43f9a55b9a8dc0b2efa0658fb81e48db8c97
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-26 18:45:09 +0900

    users/admin.py UserAdmin admin.site.register settings

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None,{
+            'fields':(
+                'username',
+                'password',
+            ),
+        }),
+        ('개인정보',{
+            'fields':(
+                'last_name',
+                'first_name',
+                'gender',
+                'email',
+                'profile_image',
+                'site_url',
+            ),
+        }),
+        ('권한', {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+
+        }),
+        ('주요 일자', {
+            'fields': (
+                'last_login',
+                'date_joined',
+            ),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
