2565cfbf7a36e74925a5d1f2a06a3a5ea1329dc1
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-27 16:23:56 +0900

    posts/urls.py comment

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
+    # root page url
     path('', views.post_list, name='post-list'),
+    # 포스팅 버튼
     path('posting/', views.post_create, name='post-create'),
+    # 해당 포스트의 아이디값을 받아서 삭제 버튼 구현
     path('<int:pk>/delete/', views.post_delete, name='post-delete')
 ]
