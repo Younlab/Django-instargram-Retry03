902ae8f6f40594ab9495afac1be2bd5c3d5ab381
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-27 14:43:29 +0900

    login decorater

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 from . import settings
@@ -26,6 +26,7 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', include('users.urls')),
 ] + static(
     prefix=settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT,
